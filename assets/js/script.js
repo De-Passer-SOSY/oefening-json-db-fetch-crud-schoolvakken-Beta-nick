d52df691aab3e79c1cdc4d4eaec2a312c88327d5
Beta-nick <nick.hernalsteen@leerling.de-passer.be>
2025-04-22 11:27:30 +0200

    functie addVak en deleteVak geschreven

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,61 @@ async function fetchVakken(){
         console.error('Fout bij het ophalen van de vakken', error);
     }
 }
+
+async function addVak(){
+    let input = document.querySelector('#vakInput');
+    let nieuweNaam = input.value.trim();
+
+    if (nieuweNaam === ''){
+        alert('Vul een vaknaam in!');
+        return;
+    }
+
+    try{
+        let response = await fetch("http://localhost:5688/vakken",{
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({naam : nieuweNaam})
+        });
+
+        if (response.ok){
+            input.value = "";
+            fetchVakken();
+        }
+    }
+    catch(error){
+        console.error('Fout bij het toevoegen', error);
+    }
+}
+
+async function deleteVak(id) {
+    try {
+        let response = await fetch(`http://localhost:5688/vakken/${id}`, {
+            method: "DELETE"
+        });
+
+        if (response.ok) {
+            fetchVakken(); // vernieuw de lijst na verwijderen
+        }
+    } catch (error) {
+        console.error("Fout bij verwijderen:", error);
+    }
+}
+
+function displayVakken(vakken) {
+    let lijst = document.querySelector("#vakList");
+    lijst.innerHTML = "";
+    vakken.forEach(vak => {
+        let li = document.createElement("li");
+        li.textContent = vak.naam;
+
+        let deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "❌";
+        deleteBtn.addEventListener("click", () => deleteVak(vak.id));
+
+        li.appendChild(deleteBtn);
+        lijst.appendChild(li);
+    });
+}
